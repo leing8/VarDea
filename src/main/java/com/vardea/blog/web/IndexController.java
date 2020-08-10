9ab084096458e6c10779e1b12956961df3004245
leing8 <leing8@163.com>
2020-08-10 13:05:37 +0800

    vardea博客第一次提交

---
+++ src/main/java/com/vardea/blog/web/IndexController.java
@@ -0,0 +1,18 @@
+package com.vardea.blog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Author Var Dea
+ * @Date 2020/8/10 13:03
+ */
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String test(){
+        return "index";
+    }
+}
